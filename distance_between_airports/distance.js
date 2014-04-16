23cb3a36a2370982191ff2ad9b5a14034eb21d4b
Adrian CB <adrian.cretu-barbul@intentmedia.com>
2014-04-16 12:37:44 -0400

    ACB: Initial terminal pricing project.

--- distance_between_airports/distance.js
+++ distance_between_airports/distance.js
@@ -1,28 +1,38 @@
 var IntentMedia = IntentMedia || {};
 
-IntentMedia.Distance = (function () {
+IntentMedia.Airports = (function () {
     var pub = {};
-    var airport_grid = airport_grid || get_airport_grid();
 
     pub.airport_exists = function (airport_code) {
-      return airport_grid.hasOwnProperty(airport_code);
+      return pub.airport_distances().hasOwnProperty(airport_code);
     };
 
-    pub.distance_between_airports = function (from_airport, to_airport) {
-      if (pub.airport_exists(from_airport) && pub.airport_exists(to_airport)) {
-        return airport_grid[from_airport][to_airport];
-      }
-
-      return 0;
-    };
-
-    function get_airport_grid() {
+    pub.airport_distances = function () {
       return {
         JFK: {LAX: 2475, LAS: 2248, PDX: 2454},
         LAX: {JFK: 2475, LAS: 236, PDX: 834},
         LAS: {JFK: 2248, LAX: 236, PDX: 763},
         PDX: {JFK: 2454, LAS: 763, LAX: 834}
       }
+    };
+
+    return pub;
+}(IntentMedia || {}));
+
+IntentMedia.Distances = (function () {
+    var pub = {};
+    var airport_distances = airport_distances || IntentMedia.Airports.airport_distances();
+
+    pub.distance_between_airports = function (from_airport, to_airport) {
+      if (IntentMedia.Airports.airport_exists(from_airport) && IntentMedia.Airports.airport_exists(to_airport)) {
+        if (from_airport === to_airport) {
+          return 0;
+        }
+
+        return airport_distances[from_airport][to_airport];
+      }
+
+      return -1;
     };
 
     return pub;
